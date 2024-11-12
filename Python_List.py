6c7c983a5d1800381b115d88cb4658c338cb313d
awillms20 <awillms17@gmail.com>
2024-11-12 17:02:28 -0500

    question1

---
+++ Python_List.py
@@ -0,0 +1,12 @@
+# Task1: given the list of grades 
+grades=[85,90,78,88,76,95,89,80,72,93]
+
+grades.sort(reverse=True)
+
+print(grades)
+
+# Task2: Calculate average
+grades_adverage= sum(grades) / len(grades)
+
+print(grades_adverage)
+
