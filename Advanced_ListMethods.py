42c516c1f7879b2f18bbe0d59cce784ad2b8fc8d
awillms20 <awillms17@gmail.com>
2024-11-12 17:24:18 -0500

    question2

---
+++ Advanced_ListMethods.py
@@ -0,0 +1,4 @@
+# Task1: Given the two list 
+submitted= ["Alice", "Bob", "Charlie", "David"]
+attended= ["Charlie", "Eve","Alice", 'Frank']
+print("Alice" in submitted) and print("Alice" in attended)
